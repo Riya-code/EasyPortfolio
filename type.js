a44a4fe4384040ec1d991b23c7b845c2d83409c9
Riya <rj220699@gmailcom>
2021-05-25 16:00:17 +0530

    update

---
+++ type.js
@@ -0,0 +1,11 @@
+var i = 0;
+var txt = 'My Portfolio'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("me").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
